6b09fd5b694165d7e82cc6d81395454b9088912d
Jerome457 <jer19801980@gmail.com>
2026-08-01 19:56:43 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDecodings(String s) {
+        int [] cache= new int [s.length()+2];
+        int n = s.length();
+        cache[n]=1;
+        for(int i=n-1;i>=0;i--){
+            char ch=s.charAt(i);
+            String number;
+            if(i!=n-1)
+                number=""+ch+s.charAt(i+1);
+            else 
+                number=""+ch;
+            if(ch=='0'){
+                cache[i]=0;
+                continue;
+            }
+            int a=Integer.parseInt(number);
+            if(a>26){
+                cache[i]=cache[i+1];
+            }
+            else
+                cache[i]=cache[i+1]+cache[i+2];
+        }
+        return cache[0];
+    }
+}
